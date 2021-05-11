cc6aa7ded2f399a5e5e889e6754d9fe67460b259
Bartha Hunor <bhun0r97@gmail.com>
2021-05-11 23:23:21 +0200

    Object.create + ES6 classes

--- Traversy_Tutorial/app.js
+++ Traversy_Tutorial/app.js
@@ -1,4 +1,4 @@
-// CONSTRUCTOR
+// CONSTRUCTOR AND THE 'THIS' KEYWORD
 
 function NewMembers (name, age, phoneNumber) {
     this.name = name;
@@ -80,4 +80,79 @@ Customer.prototype.constructor = Customer;
 Customer.prototype.greeting = function() {
     return `Hello there ${this.firstName} ${this.lastName} Welcome to our company!`
 }
-console.log(customer1.greeting())
+// // console.log(customer1.greeting())
+
+
+// -----------------------------------------------------
+// USING 'OBJECT.CREATE' - ES5 - ("not the easiest way")
+const emptyObject = {
+}
+
+const john = Object.create(emptyObject);
+john.firstName = "John";
+john.middleName = "Daniel"
+john.lastName = "Baldwin"
+john.age = 40;
+// // console.log(John)
+
+
+const personPrototypes = {
+    greeting: function() {
+        return `Hello there ${this.firstName} ${this.middleName} ${this.lastName}, who is ${this.age}-years-old`;
+    },
+    nameChange: function(newLastName) {
+        this.lastName = newLastName;
+    }
+}
+
+const brad = Object.create(personPrototypes);
+brad.firstName = "Brad";
+brad.middleName  = "Davis"
+brad.lastName = "McLane"
+brad.age = 35;
+
+brad.nameChange("Bobson");
+
+// Don't forget the '()' after the function'
+// // console.log(brad.greeting());
+
+// Another way of creating Objects
+const george = Object.create(personPrototypes, {
+    firstName: {value: "George"},
+    lastName: {value: "Antonio"},
+    age: {value: 35}
+});
+// // console.log(george)
+
+
+// -----------------------------------------------------
+// ES6 CLASSES
+class Person_2 {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dob);
+    }
+    greeting() {
+        return `Hi there ${this.firstName} ${this.lastName}!`
+    }
+    // Note: Revisit this 'age' section below
+    calculateAge() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+    // Note: Revisit this 'age' section above
+    getsMarried(newLastName) {
+        this.lastName = newLastName;
+    }
+    // static methods (a standalone method in your class that doesn't use the 'this' for example)
+    static addNumbers(x, y) {
+        return x + y;
+    }
+}
+const dave = new Person_2("Dave", "Johnson")
+dave.getsMarried("Anderson")
+console.log(dave.greeting())
+console.log(Person_2.addNumbers(5,7))
+
