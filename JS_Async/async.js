e8fbd79cafe146b23b9db4dc06265df05b029b2c
Bartha Hunor <bhun0r97@gmail.com>
2021-05-19 21:58:40 +0200

    async / await - updated

--- JS_Async/async.js
+++ JS_Async/async.js
@@ -18,3 +18,31 @@ function processRequest(response) {
     resolve(`Extra Information + ${response}`);
   });
 }
+
+// SIMPLIFYING THIS SECTION - does the same thing tho
+// makeRequest("Goole")
+//   .then((response) => {
+//     console.log("Response Reveived");
+//     return provessRequest(response);
+//   })
+//   .then((processedResponse) => {
+//     console.log(processedResponse);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+async function doWork() {
+  // AWAIT - The code should wait until this 'makeRequest' is finished...
+  // ...and then afterwards it will execute the next thing
+  try {
+    // TRY - put all the code inside that could potentially fail
+    const response = await makeRequest("Google");
+    console.log("Response Received");
+    const processedResponse = await processRequest(response);
+    console.log(processedResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+doWork();
