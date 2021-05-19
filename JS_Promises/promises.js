27598230200741a325aa7ab08878ec04a797b291
Bartha Hunor <bhun0r97@gmail.com>
2021-05-19 18:32:51 +0200

    Promise.all / Promise.race

--- JS_Promises/promises.js
+++ JS_Promises/promises.js
@@ -1,24 +1,49 @@
-let newPromise = new Promise((resolve, reject) => {
-  // define what the actual promise is
-  let a = 1 + 1;
-  // define what the actual promise is
-  if (a == 3) {
-    resolve("Success!");
-  } else {
-    reject("Failed!");
-  }
+// let newPromise = new Promise((resolve, reject) => {
+//   // define what the actual promise is
+//   let a = 1 + 1;
+//   if (a == 3) {
+//     resolve("Success!");
+//   } else {
+//     reject("Failed!");
+//   }
+// });
+
+// // .THEN - this is for RESOLVE - "do this when it succeeds"
+// newPromise.then((message) => {
+//   // message - here it's resolve("success!")
+//   console.log(`This is in the "then" ${message}`);
+// });
+
+// // .CATCH - this is for REJECT - "do this when it fails"
+// newPromise.catch((message) => {
+//   // message - here it's reject("Failed!")
+//   console.log(`This is in the "catch" ${message}`);
+// });
+// // console.log(newPromise);
+
+// ----------------------------------------------------------------
+const promise1 = new Promise((resolve, reject) => {
+  resolve("Resolve Message_1!");
 });
 
-// .THEN - this is for RESOLVE - if the promise runs successfully
-newPromise.then((message) => {
-  // message - here it's resolve("success!")
-  console.log(`This is in the "then" ${message}`);
+const promise2 = new Promise((resolve, reject) => {
+  resolve("Resolve Message_2!");
 });
 
-// .CATCH - this is for REJECT - if the promise fails
-newPromise.catch((message) => {
-  // message - here it's reject("Failed!")
-  console.log(`This is in the "catch" ${message}`);
+const promise3 = new Promise((resolve, reject) => {
+  resolve("Resolve Message_3!");
 });
 
-console.log(newPromise);
+// PROMISE.ALL - inside we send it in an ARRAY
+// of all the diff promises we want to run.
+// These are all run at the exact same time
+let promAll = Promise.all([promise1, promise2, promise3]).then((messages) => {
+  console.log(`These are the resolve messages: ${messages}`);
+});
+
+// ----------------------------------------------------------------
+// PROMISE.RACE - it will return as soon as the first one is completed
+// instead of waiting for everything to complete
+let promRace = Promise.race([promise1, promise2, promise3]).then((messages) => {
+  console.log(`The first resolve message: ${messages}`);
+});
